11fdeaf78738f4073ca6b55090dbc6d06f17f06f
AkaY <98938792+aliffathoni@users.noreply.github.com>
2022-09-27 14:58:24 +0700

    Update GetChipID.ino

--- example/GetChipID/GetChipID.ino
+++ example/GetChipID/GetChipID.ino
@@ -3,16 +3,15 @@
 ESP32ChipID esp;
 
 void setup() {
-  esp.begin();
+  esp.begin();  //Optional, starting serial comm with 115200 default baudrate
 }
 
 void loop() {
-	Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
-	Serial.printf("This chip has %d cores\n", ESP.getChipCores());
+  Serial.print("Chip ID: "); Serial.println(esp.getChipId());
   Serial.print("Chip ID 64 bit: "); Serial.println(esp.getChipId12());
-  Serial.print("Chip ID 32 bit: "); Serial.println(esp.getChipId());
+  Serial.print("Chip ID 32 bit: "); Serial.println(esp.getChipId8());
   Serial.print("Chip ID 16 bit: "); Serial.println(esp.getChipId3());
   Serial.print("Chip ID 8 bit: "); Serial.println(esp.getChipId2());
   
-	delay(3000);
+  delay(3000);
 }
